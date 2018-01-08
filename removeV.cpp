09fc749ac8b46ad10b340a06660aa750f1c9e095
Ulrica <393060556@qq.com>
2018-01-08 16:02:09 +0800

    123

--- removeV.cpp
+++ removeV.cpp
@@ -2,10 +2,12 @@
 template<class T>
 void RemoveV(Router<T> &G)
 {
-	cout << endl << "ÊäÈëÉ¾³ýµÄ¶¥µã£¨¼´Â·ÓÉÆ÷ÐòºÅ£©£º";
+	cout << "ÁõÓîÇç" << endl;
+	cout <<endl<< "ÊäÈëÉ¾³ýµÄ¶¥µã£¨¼´Â·ÓÉÆ÷ÐòºÅ£©£º";
 	int v;
 	cin >> v;
-	if (G.removeVertex(G.getVertexPos(v)) == true) {
+	if (G.removeVertex(G.getVertexPos(v)) == true)
+	{
 		cout << "É¾³ý³É¹¦!" << endl;
 		return;
 	}
