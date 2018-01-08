f6148f5d46530b3175c4bd588d00e63e7b49e626
Ulrica <393060556@qq.com>
2018-01-08 10:57:29 +0800

    debug1

--- Router.cpp
+++ Router.cpp
@@ -22,7 +22,7 @@ void Print(Router<T> &G, int v)
 	for (int i = 0; i < G.NumberOfVertices(); i++)
 	{
 		if (i != v)
-			cout << " 目的路由器 ： R" << G.getValue(i) << "      下一跳 ：R" << path[i] << endl;
+			cout << " 目的路由器 ： R" << G.getValue(i) << "      下一跳 ：R" << G.getValue(path[i])<< endl;
 	}
 }
 template<class T>
@@ -35,14 +35,14 @@ void Remove(Router<T> &G)
 		cout << endl << "输入删除的顶点（即路由器序号）：";
 		int v;
 		cin >> v;
-		if (G.removeVertex(G.getVertexPos(v)))
+		if (G.removeVertex(v))
 			return;
 	}
 	else {
 		cout << endl << "输入删除的边（即哪两个路由器序号，用空格分开）：";
 		int v1, v2;
 		cin >> v1 >> v2;
-		if (G.removeEdge(G.getVertexPos(v1), G.getVertexPos(v2)))
+		if (G.removeEdge(v1, v2))
 			return;
 	}
 
@@ -73,10 +73,9 @@ int main() {
 	int  vv;
 	cout << "请输入路由器" << endl;
 	cin >> vv;
-	int pos = g1.getVertexPos(vv);
-	Print(g1, pos);
+	Print(g1, vv);
 	Remove(g1);
-	Print(g1, pos);
+	Print(g1, vv);
 	system("pause"); 
 }
 
